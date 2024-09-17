ea5c4abc98757609984093f7600bc8acb3889c54
종혁 <139520014+GGomsoo@users.noreply.github.com>
2024-09-17 20:03:48 +0900

    [Bronze III] Title: 부호, Time: 164 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1247. 부호/부호.py
+++ 백준/Bronze/1247. 부호/부호.py
@@ -1,10 +1,11 @@
 # 부호
 # 브론즈 III
 # N개 정수 주어지면, 정수들의 합 S의 부호 구하기
+import sys; input = sys.stdin.readline
 
 tc = 3
 for _ in range(tc):
-    N = int(input())
+    N = int(input().strip())
 
     total = 0
     for _ in range(N):
