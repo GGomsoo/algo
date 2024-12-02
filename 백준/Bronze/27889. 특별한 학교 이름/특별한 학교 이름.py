f84c3e0fedead8d0f6f6b6ae9a154d33674eb784
종혁 <139520014+GGomsoo@users.noreply.github.com>
2024-12-02 20:45:40 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+school_name = input()
+
+if school_name == "NLCS":
+    print("North London Collegiate School")
+elif school_name == "BHA": 
+    print("Branksome Hall Asia")
+elif school_name == "KIS":
+    print("Korea International School")
+else:
+    print("St. Johnsbury Academy")
