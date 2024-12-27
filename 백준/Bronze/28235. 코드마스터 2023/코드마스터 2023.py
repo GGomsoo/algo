287ee910fdd7b522e818a2899b7f096bf5275811
종혁 <139520014+GGomsoo@users.noreply.github.com>
2024-12-28 01:59:18 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,13 @@
+# 코드마스터 2023
+# 브론즈 V
+
+sign = input()
+
+if sign == "SONGDO":
+    print("HIGHSCHOOL")
+elif sign == "CODE":
+    print("MASTER")
+elif sign == "2023":
+    print("0611")
+else:
+    print("CONTEST")
