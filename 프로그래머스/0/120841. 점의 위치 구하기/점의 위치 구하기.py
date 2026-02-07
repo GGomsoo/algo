4a97db57138fe257754dddf0ec6a203b3027f83b
종혁 <139520014+GGomsoo@users.noreply.github.com>
2026-02-08 01:01:59 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -1,12 +1,14 @@
 def solution(dot):
     answer = 0
+    x = dot[0]
+    y = dot[1]
     
     for _ in dot:
-        if dot[0] > 0 and dot[1] > 0:
+        if x > 0 and y > 0:
             answer = 1
-        elif dot[0] < 0 and dot[1] > 0:
+        elif x < 0 and y > 0:
             answer = 2
-        elif dot[0] < 0 and dot[1] < 0:
+        elif x < 0 and y < 0:
             answer = 3
         else:
             answer = 4
